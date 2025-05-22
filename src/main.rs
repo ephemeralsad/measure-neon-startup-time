15abf34f32a8a656b7994eb76bd74830db4e23a6
Aleksandr Sarantsev <aleksandr@neon.tech>
2025-05-22 17:53:48 +0400

    add suspend (works badly)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use openssl::ssl::{SslConnector, SslMethod};
-use postgres::Client;
 use postgres_openssl::MakeTlsConnector;
 use std::error;
 
-fn write_n_rows(client: &mut Client, table: &str, count: i32) -> Result<(), Box<dyn error::Error>> {
+fn write_n_rows(client: &mut postgres::Client, table: &str, count: i32) -> Result<(), Box<dyn error::Error>> {
     assert!(count > 0, "Count must be greater than 0");
 
     println!("Dropping table {table} if exists...");
@@ -33,7 +32,7 @@ struct NeonResponse {
 }
 
 fn make_read_query(
-    client: &mut Client,
+    client: &mut postgres::Client,
     table: &str,
 ) -> Result<Vec<NeonResponse>, Box<dyn error::Error>> {
     let mut ret = Vec::<NeonResponse>::new();
@@ -48,28 +47,61 @@ fn make_read_query(
     Ok(ret)
 }
 
+fn suspend_compute(api_key: &str, project_id: &str, server_name: &str) -> Result<(), Box<dyn error::Error>> {
+    let url = format!(
+        "https://console-stage.neon.build/api/v2/projects/{}/endpoints/{}/suspend",
+        project_id, server_name
+    );
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .post(&url)
+        .bearer_auth(api_key)
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()?;
+
+    println!("Suspend status code: {}", response.status());
+    println!("Suspend response: {}", response.text()?);
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mode = std::env::args()
         .nth(1)
-        .unwrap_or_else(|| "read".to_string());
+        .unwrap();
+
+    // Set up Neon connection parameters
+    let neon_server_name =
+        std::env::var("NEON_SERVER_NAME").unwrap_or_else(|_| "ep-soft-mouse-w1azk5bq-pooler".to_string());
+    let neon_server_domain =
+        std::env::var("NEON_SERVER_DOMAIN").unwrap_or_else(|_| "eu-west-1.aws.neon.build".to_string());
+    let neon_endpoint = format!("{neon_server_name}.{neon_server_domain}");
+
+    // Set up Neon credentials
     let neon_username =
         std::env::var("NEON_USERNAME").unwrap_or_else(|_| "neondb_owner".to_string());
     let neon_password =
         std::env::var("NEON_PASSWORD").expect("NEON_PASSWORD must be set to environment variable");
-    let table = std::env::var("TABLE_NAME").unwrap_or_else(|_| "test_table".to_string());
+    let neon_api_key =
+        std::env::var("NEON_API_KEY").expect("NEON_API_KEY must be set to environment variable");
+
+    // Set up Neon project & table
+    let neon_project =
+        std::env::var("NEON_PROJECT").unwrap_or_else(|_| "hello-world".to_string());
+    let table =std::env::var("TABLE_NAME").unwrap_or_else(|_| "test_table".to_string());
+
+    let connection_string: String = format!(
+        "postgresql://{}:{}@{}/neondb?sslmode=require",
+        neon_username, neon_password, neon_endpoint
+    );
 
     let builder = SslConnector::builder(SslMethod::tls())?;
     let connector: MakeTlsConnector = MakeTlsConnector::new(builder.build());
-    let connection_string: String = format!(
-        "postgresql://{}:{}@ep-dry-leaf-w3c56xzk-pooler.eastus2.azure.neon.build/neondb?sslmode=require",
-        neon_username, neon_password
-    );
-
-    let mut client = Client::connect(&connection_string, connector)?;
 
     match mode.as_str() {
         "read" => {
             println!("Running in read mode");
+            let mut client = postgres::Client::connect(&connection_string, connector)?;
             for i in 1..=10 {
                 let start_time = std::time::Instant::now();
                 let result = make_read_query(&mut client, &table)?;
@@ -80,7 +112,12 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         }
         "write" => {
             println!("Running in write mode");
-            write_n_rows(&mut client, &table, 300000)?;
+            let mut client = postgres::Client::connect(&connection_string, connector)?;
+            write_n_rows(&mut client, &table, 30000)?;
+        }
+        "suspend" => {
+            println!("Running in suspend mode");
+            suspend_compute(&neon_api_key, &neon_project, &neon_server_name)?;
         }
         _ => {
             println!("Invalid mode. Use 'read' or 'write'.");
